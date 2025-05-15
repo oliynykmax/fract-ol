f4949551e91572c51358979cdea3acee8273cf56
oliynykmax <olejnikmax@gmail.com>
2025-05-15 14:47:53 +0300

    now even better

--- Makefile
+++ Makefile
@@ -6,9 +6,10 @@
 #    By: maoliiny <maoliiny@student.hive.fi>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/05/09 19:29:32 by maoliiny          #+#    #+#              #
-#    Updated: 2025/05/15 12:41:36 by maoliiny         ###   ########.fr        #
+#    Updated: 2025/05/15 14:28:31 by maoliiny         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
+
 
 NAME = fract-ol
 
@@ -18,12 +19,16 @@ CFLAGS = -Wall -Wextra -Werror
 SRCS = srcs/fract-ol.c
 OBJS = $(SRCS:.c=.o)
 
-INCLUDES = -Iincl -Iassets/libft
+INCLUDES = -Iincl -Iassets/libft -Iassets/MLX42/include
 MLX42 = assets/MLX42/build/libmlx42.a
+.SECONDARY : MLX42
+all: $(MLX42) assets/libft/libft.a $(NAME)
 
-all: $(NAME)
+$(MLX42):
+	@cmake -S assets/MLX42 -B assets/MLX42/build
+	@make -C assets/MLX42/build -j4
 
-$(NAME): $(OBJS) assets/libft/libft.a
+$(NAME): $(OBJS) assets/libft/libft.a $(MLX42)
 	$(CC) $(CFLAGS) $(INCLUDES) $(OBJS) assets/libft/libft.a $(MLX42) -ldl -lglfw -pthread -lm -o $(NAME)
 
 assets/libft/libft.a:
@@ -32,20 +37,20 @@ assets/libft/libft.a:
 srcs/%.o: srcs/%.c
 	$(CC) $(CFLAGS) $(INCLUDES) -c $< -o $@
 
-# Ensure object files are rebuilt if headers change
 srcs/fract-ol.o: srcs/fract-ol.c incl/fract-ol.h
 
 clean:
-	rm -f $(OBJS)
+	@rm -f $(OBJS)
 	@make clean -C assets/libft
+	@rm -rf assets/MLX42/build
 
 fclean: clean
-	rm -f $(NAME)
-	rm -f assets/libft/libft.a
+	@rm -f $(NAME)
 	@make fclean -C assets/libft
 
 re: fclean all
 
 .PHONY: all clean fclean re
+
 
 
--- incl/fract-ol.h
+++ incl/fract-ol.h
@@ -1,61 +1,40 @@
 #ifndef FRACT_OL_H
-#define FRACT_OL_H
+# define FRACT_OL_H
 
-#include "../assets/MLX42/include/MLX42/MLX42.h"
-#include "../assets/libft/libft.h"
-#include <math.h>
-#include <stdlib.h>
-#define EXIT_STR "The parameter is invalid. Available parameters are \"Julia\" and \"Mandelbrot\", you can also pass number as a second parameter, to do some changes to the rendering"
-#define SIZE 500
-
+# include "../assets/MLX42/include/MLX42/MLX42.h"
+# include "../assets/libft/libft.h"
+# include <math.h>
+# include <stdlib.h>
+#define EXIT_STR "Usage:\n\
+./fractol Mandelbrot\n\
+./fractol Julia\n\
+./fractol Julia <real> <imaginary>\n\
+Example: ./fractol Julia -0.4 0.6"
+# define SIZE 500
 
 typedef struct s_palette
 {
-    uint8_t count;
-    int cycle;
-    int colors[16];
+	uint8_t count;
+	int cycle;
+	int colors[16];
 } t_palette;
-
-
-// typedef struct s_fractal
-// {
-//     mlx_t           *mlx;
-//     mlx_image_t     *g_img;
-//     double          k;
-//     double          h;
-//     double          z_re;
-//     double          z_im;
-//     double          c_re;
-//     double          c_im;
-//     double          zoom;
-//     double          y;
-//     double          i;
-//     double          x;
-//     double          limx;
-//     double          limy;
-//     double          max_iter;
-//     t_palette       *palette;
-//     t_palette       *palettes;
-//     int                     palette_index;
-//     int                     smooth;
-//     int                     palette_len;
-//     char                    *f_type;
-// }                               t_fractal;
 
 typedef struct s_fractal
 {
-    void* mlx;
-    void* g_img;
-    char* f_type;
-    int x;
-    int y;
-    double real;
-    double imag;
-    double zoom;
-    double shift_x; // For panning horizontally
-    double shift_y; // For panning vertically
-    int max_iter;
-    int i;
+	void* mlx;
+	void* g_img;
+	char* f_type;
+	int x;
+	int y;
+	double real;
+	double imag;
+	double zoom;
+	double shift_x;
+	double shift_y;
+	int max_iter;
+	int i;
+	double julia_real;
+	double julia_imag;
 } t_fractal;
 
 void init(t_fractal* f);
